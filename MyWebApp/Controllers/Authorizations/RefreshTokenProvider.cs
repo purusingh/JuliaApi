9acb89ad36c2951752dedd1ab9e433488634b17b
purusingh <purusingh@hotmail.com>
2022-12-26 11:58:57 -0500

    #8 refresh token implemented

---
+++ MyWebApp/Controllers/Authorizations/RefreshTokenProvider.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyWebApp.Controllers.Authorizations
+{
+    public class RefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens = new ConcurrentDictionary<string, AuthenticationTicket>();
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            // Set lifetime of refresh token 
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary)
+            {
+                IssuedUtc = context.Ticket.Properties.IssuedUtc,
+                ExpiresUtc = DateTime.UtcNow.AddMinutes(60)
+            };
+
+            var refreshTokenTicket = new AuthenticationTicket(context.Ticket.Identity, refreshTokenProperties);
+
+            _refreshTokens.TryAdd(guid, refreshTokenTicket);
+
+            context.SetToken(guid);
+            return Task.FromResult<object>(null);
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            AuthenticationTicket ticket;
+            string header = context.OwinContext.Request.Headers["Authorization"];
+
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+            return Task.FromResult<object>(null);
+        }
+    }
+}
